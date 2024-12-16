648b7e1a4056e1cbafb76b2c3e31203e19537030
Thibaud-LAMON <thib.lamon@gmail.com>
2024-12-16 16:59:32 +0100

    in memory authentication

--- src/main/java/fr/lamon/bde/commandes/security/SecurityConfig.java
+++ src/main/java/fr/lamon/bde/commandes/security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,14 +18,33 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         //authentifie toutes les requêtes http venant vers l'appli
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         //désactive la gestion des sessions
-        //http.formLogin(withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //authentification par formulaire avec les valeurs par défaut
-        http.httpBasic(withDefaults());
+        //http.formLogin(withDefaults());
         //authentification basique avec les valeurs par défaut
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    /**
+     * permet de gérer l'authentification in memory
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user")
+                .password("{noop}password") //noop signifie que le mot de passe n'est pas crypté
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+
+        //l'objet aura comme paramètres les credentials de l'utilisatisateur
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
